64e7e3c365ff2e07ed8d820d1d8e70f2dedee569
adnan77ai <yasinadnan79@gmail.com>
2022-11-11 14:50:18 +0600

    initialized

---
+++ naming_variables.py
@@ -0,0 +1,7 @@
+myName = input("What's you name?: ")
+print("WoW....😍 Nice name")
+myAge = input("How old are you? :")
+print("So you are", myAge, "old")
+git = input("Do you like git?")
+print("OF COURSE YOU DO!")
+
